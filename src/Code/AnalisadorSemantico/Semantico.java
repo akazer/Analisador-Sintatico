6135567114128754817e5272412205d26287f30d
Leonardo Santana <Leonardo Santana@192.168.15.147>
2016-10-31 19:14:35 -0300

    Merge origin/master

--- src/Code/AnalisadorSemantico/Semantico.java
+++ src/Code/AnalisadorSemantico/Semantico.java
@@ -8,7 +8,7 @@ package Code.AnalisadorSemantico;
 import Code.Token;
 import java.util.ArrayList;
 import java.util.List;
-import sun.reflect.generics.tree.Tree;
+import java.util.LinkedHashMap;
 
 //Vohayoooo!
 
@@ -18,12 +18,13 @@ public class Semantico {
     List<Token> outraList;
     Integer i;
     List<String> erros;
-    Tree escopos;
+    LinkedHashMap escopos;
     ArrayList<Simbolo> tabelaSimbolos;
     
 
     public Semantico(List<Token> l){
         list = l;
+        tabelaSimbolos = new ArrayList<>();
         list.add(new Token("$","$",l.get(l.size()-1).getLinha()));
         i = 0;
         erros = new ArrayList<>();
@@ -32,9 +33,49 @@ public class Semantico {
     }
     
     public void preRun(){
-        
-        //testeeeeeeeeeeeeeeeeeeeeeeeeeeeeee porraaaaaaaaaaaaa sem palavrao por favor
-        
+        int j = 0; //escopo Atual
+        for(int pos = 0; pos < outraList.size();pos++){
+            Token t = outraList.get(pos);
+            
+            if(t.getLexema().equals("inicio")){
+                j++; //isso ta errado
+            }else if(t.getLexema().equals("fim")){
+                j--; //isso ta errado
+            } else if(t.getTipo().equals("identificador")){
+                //TODO 
+            } else if(t.getTipo().equals("numero")){
+                if(t.getLexema().contains(".")){
+                    t.setTipo("numero_real");
+                } else {
+                    t.setTipo("numero_inteiro");
+                }
+            } else if(t.getLexema().equals("var")){
+                String tipo = outraList.get(++pos).getLexema();
+                while(!t.getLexema().equals(";")){                    
+                    t = outraList.get(++pos);
+                    if(t.getTipo().equals("identificador")){
+                        tabelaSimbolos.add(new Simbolo(t.getLexema(), tipo, "variavel", j));
+                    }
+                }
+                
+            } else if(t.getLexema().equals("const")){
+                String tipo = outraList.get(++pos).getLexema();
+                while(!t.getLexema().equals(";")){                    
+                    t = outraList.get(++pos);
+                    if(t.getTipo().equals("identificador")){
+                        tabelaSimbolos.add(new Simbolo(t.getLexema(), tipo, "constante", j));
+                    }
+                }
+            } else if(t.getLexema().equals("funcao")){
+                String tipo = outraList.get(++pos).getLexema();
+                while(!t.getLexema().equals(";")){                    
+                    t = outraList.get(++pos);
+                    if(t.getTipo().equals("identificador")){
+                        tabelaSimbolos.add(new Simbolo(t.getLexema(), tipo, "constante", j));
+                    }
+                }
+            }
+        }
     }
     
 }
