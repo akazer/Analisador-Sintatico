8d49e383dde873b74dcd57b60d13455617323b35
Leonardo Santana <Leonardo Santana@192.168.15.147>
2016-11-01 14:49:48 -0300

    try

--- src/Code/AnalisadorSemantico/Simbolo.java
+++ src/Code/AnalisadorSemantico/Simbolo.java
@@ -5,6 +5,8 @@
  */
 package Code.AnalisadorSemantico;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Simbolo {
@@ -14,6 +16,7 @@ public class Simbolo {
     String tipo;
     int escopo;
     List<String> parametros;
+    List<Integer> dimensoes;
     
     public Simbolo(String nome, String tipoId, String tipo, int escopo){
         
@@ -21,12 +24,18 @@ public class Simbolo {
         this.tipoId = tipoId;
         this.tipo = tipo;
         this.escopo = escopo;
+        parametros = new ArrayList<>();
+        dimensoes = new ArrayList<>();
     }
     
     public void addParametro(String tipoParametro){
         
         this.parametros.add(tipoParametro);
         
+    }
+    
+    public void addDimensoes(int n){
+        dimensoes.add(n);
     }
     
 }
